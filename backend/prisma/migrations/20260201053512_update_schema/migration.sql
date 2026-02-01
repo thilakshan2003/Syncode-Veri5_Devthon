eb05c381856982765a998838d35ae9d9edfed21c
YohanJaya <nyajayasinghedmc10@gmail.com>
2026-02-01 11:30:56 +0530

    Fix some conflicts

--- backend/prisma/migrations/20260201053512_update_schema/migration.sql
+++ backend/prisma/migrations/20260201053512_update_schema/migration.sql
@@ -6,12 +6,30 @@
   - You are about to drop the column `role` on the `User` table. All the data in the column will be lost.
   - You are about to drop the `Comment` table. If the table is not empty, all the data it contains will be lost.
   - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `appointment_slots` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `appointments` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `clinic_staff` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `clinics` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `order_items` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `orders` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `partners` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `payments` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `practitioner_clinics` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `practitioners` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `shipments` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `status_shares` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `test_kits` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `test_types` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user_current_status` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user_profile` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user_verifications` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
   - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
   - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
 -- CreateEnum
-CREATE TYPE "UserStatus" AS ENUM ('VERIFIED', 'UNVERIFIED', 'PENDING');
+CREATE TYPE "UserStatusNew" AS ENUM ('VERIFIED', 'UNVERIFIED', 'PENDING');
 
 -- CreateEnum
 CREATE TYPE "TestStatus" AS ENUM ('PENDING', 'VALIDATED', 'REJECTED');
@@ -31,6 +49,81 @@ ALTER TABLE "Comment" DROP CONSTRAINT "Comment_postId_fkey";
 -- DropForeignKey
 ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
 
+-- DropForeignKey
+ALTER TABLE "appointment_slots" DROP CONSTRAINT "appointment_slots_clinic_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "appointment_slots" DROP CONSTRAINT "appointment_slots_practitioner_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "appointments" DROP CONSTRAINT "appointments_slot_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "appointments" DROP CONSTRAINT "appointments_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "clinic_staff" DROP CONSTRAINT "clinic_staff_clinic_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "clinic_staff" DROP CONSTRAINT "clinic_staff_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "order_items" DROP CONSTRAINT "order_items_order_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "order_items" DROP CONSTRAINT "order_items_test_kit_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "orders" DROP CONSTRAINT "orders_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "partners" DROP CONSTRAINT "partners_received_by_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "partners" DROP CONSTRAINT "partners_shared_by_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "payments" DROP CONSTRAINT "payments_appointment_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "payments" DROP CONSTRAINT "payments_order_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "practitioner_clinics" DROP CONSTRAINT "practitioner_clinics_clinic_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "practitioner_clinics" DROP CONSTRAINT "practitioner_clinics_practitioner_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "practitioners" DROP CONSTRAINT "practitioners_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "shipments" DROP CONSTRAINT "shipments_order_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "status_shares" DROP CONSTRAINT "status_shares_recipient_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "status_shares" DROP CONSTRAINT "status_shares_sender_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "user_current_status" DROP CONSTRAINT "user_current_status_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "user_profile" DROP CONSTRAINT "user_profile_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "user_verifications" DROP CONSTRAINT "user_verifications_clinic_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "user_verifications" DROP CONSTRAINT "user_verifications_test_type_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "user_verifications" DROP CONSTRAINT "user_verifications_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "user_verifications" DROP CONSTRAINT "user_verifications_verified_by_user_id_fkey";
+
 -- AlterTable
 ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
 DROP COLUMN "name",
@@ -41,7 +134,7 @@ ADD COLUMN     "expiryDate" TIMESTAMP(3),
 ADD COLUMN     "firstName" TEXT NOT NULL,
 ADD COLUMN     "gender" TEXT,
 ADD COLUMN     "lastName" TEXT NOT NULL,
-ADD COLUMN     "status" "UserStatus" NOT NULL DEFAULT 'UNVERIFIED',
+ADD COLUMN     "status" "UserStatusNew" NOT NULL DEFAULT 'UNVERIFIED',
 ALTER COLUMN "id" DROP DEFAULT,
 ALTER COLUMN "id" SET DATA TYPE TEXT,
 ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
@@ -53,8 +146,89 @@ DROP TABLE "Comment";
 -- DropTable
 DROP TABLE "Post";
 
+-- DropTable
+DROP TABLE "appointment_slots";
+
+-- DropTable
+DROP TABLE "appointments";
+
+-- DropTable
+DROP TABLE "clinic_staff";
+
+-- DropTable
+DROP TABLE "clinics";
+
+-- DropTable
+DROP TABLE "order_items";
+
+-- DropTable
+DROP TABLE "orders";
+
+-- DropTable
+DROP TABLE "partners";
+
+-- DropTable
+DROP TABLE "payments";
+
+-- DropTable
+DROP TABLE "practitioner_clinics";
+
+-- DropTable
+DROP TABLE "practitioners";
+
+-- DropTable
+DROP TABLE "shipments";
+
+-- DropTable
+DROP TABLE "status_shares";
+
+-- DropTable
+DROP TABLE "test_kits";
+
+-- DropTable
+DROP TABLE "test_types";
+
+-- DropTable
+DROP TABLE "user_current_status";
+
+-- DropTable
+DROP TABLE "user_profile";
+
+-- DropTable
+DROP TABLE "user_verifications";
+
+-- DropTable
+DROP TABLE "users";
+
+-- DropEnum
+DROP TYPE "AppointmentSlotMode";
+
+-- DropEnum
+DROP TYPE "AppointmentStatus";
+
+-- DropEnum
+DROP TYPE "ClinicStaffRole";
+
+-- DropEnum
+DROP TYPE "OrderStatus";
+
+-- DropEnum
+DROP TYPE "PartnerStatus";
+
+-- DropEnum
+DROP TYPE "PaymentStatus";
+
 -- DropEnum
 DROP TYPE "Role";
+
+-- DropEnum
+DROP TYPE "ShipmentStatus";
+
+-- DropEnum
+DROP TYPE "UserStatus";
+
+-- DropEnum
+DROP TYPE "VerificationStatus";
 
 -- CreateTable
 CREATE TABLE "Test" (
