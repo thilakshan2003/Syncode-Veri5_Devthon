6650358c0c5cf89fbcc5ddabe34b5c97e061be5e
sheronjay <192441684+sheronjay@users.noreply.github.com>
2026-02-09 22:52:29 +0530

    remove previous migrations and create one single migration and improve db seeder

--- backend/prisma/migrations/20250202001100_create_user_current_status/migration.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "user_current_status" (
-    "user_id" BIGINT PRIMARY KEY,
-    "status" "VerificationStatus" NOT NULL DEFAULT 'unverified',
-    "last_verified_at" TIMESTAMP,
-    "updated_at" TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT "user_current_status_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE
-);
